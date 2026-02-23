82bf850d38e547a108a289ad39de132c128cf326
Amit Rajput <amitrajput98267313@gmail.com>
2026-02-23 13:48:15 +0530

    fix: update views path to absolute for Vercel deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const errorController = require("./controllers/errors");
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
 app.use(storeRouter);
